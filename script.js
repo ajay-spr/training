7ddcc19d86a37af012d5536f85bab2f01574127c
Ajay Yadav <ajay.yadav@sprinklr.com>
2023-07-19 14:19:35 +0530

    comparisons

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Comparisons
 
 
-console.log(5 > 3)
-console.log("apple" > "banana")
-console.log("2" > "212")
-console.log(undefined == null)
-console.log(undefined === null)
-console.log(null == "\n0\n")
-console.log(null == +"\n0\n")
+console.log(5 > 3) // t
+console.log("apple" > "banana") // f
+console.log("2" > "212") // f
+console.log(undefined == null) // t
+console.log(undefined === null) // f
+console.log(null == "\n0\n") // f
+console.log(null == +"\n0\n") // f
